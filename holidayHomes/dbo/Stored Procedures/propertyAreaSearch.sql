0e730e1c7924a037afe841ea90cbf18d8b996c28
Tim Wilson <tim.wilson@compareandshare.com>
2014-03-28 16:03:45 +0000

    1. New holidayHomes table tab_propertyLatLong 2. New area focussed property search proc propertyAreaSearch using new table tab_propertyLatLong 3. Added reference to master database to holidayHomes and holidayHomes_build to stop SQL71502 warning messages about proc proc_db_tableusage

---
+++ holidayHomes/dbo/Stored Procedures/propertyAreaSearch.sql
@@ -0,0 +1,208 @@
+﻿-- =============================================
+-- Author:	James Privett
+-- Create date: 09/12/2013
+-- Description:	Search for a property
+--
+-- History
+--	2014-01-08 MC added nullable params, dynamic order by (needs more), totalCount, OFFSET and FETCH pagination
+--  2014-01-08 JP added pro.sourceId, pro.externalId to select statement
+--  2014-01-10 JP added pro.minimumPricePerNight to select statement
+--  2014-02-20 TW added parameter sourceIds (null or single sourceId or comma delimited list of sourceIds)
+--                requires the utils_numbers table
+--  2014-03-07 TW added parameter numberOfBedrooms
+--  2014-03-13 TW added parameter countryCode
+--  2014-03-27 TW added parameters for latitude / longitude / radius / imperial units
+--  2014-03-28 TW renamed as propertyAreaSearch to preserve the original propertySearch
+-- =============================================
+CREATE PROCEDURE [dbo].[propertyAreaSearch]
+-- Add the parameters for the stored procedure here
+  @searchCriteria VARCHAR(150) = NULL
+, @typeOfProperty VARCHAR(15) = NULL
+, @countryCode VARCHAR(2) = NULL
+, @sleeps int = 1
+, @numberOfBedrooms int = NULL
+, @sourceIds varchar(MAX) = NULL
+, @centralLatitude float
+, @centralLongitude float
+, @swLatitude float
+, @swLongitude float
+, @neLatitude float
+, @neLongitude float
+, @radius decimal(18,2) = 0
+, @imperial bit = 0
+, @Page int
+, @RecsPerPage int
+, @orderBy VARCHAR(15) = 'beds'
+, @orderDESC BIT = 0
+AS
+BEGIN
+ -- SET NOCOUNT ON added to prevent extra result sets from
+ -- interfering with SELECT statements.
+ SET NOCOUNT ON;
+
+ DECLARE @sourceId int
+	, @conversion decimal(10,4);
+
+ IF @sourceIds IS NOT NULL AND CHARINDEX(',', @sourceIds, 0) = 0
+ BEGIN
+  SET @sourceId = CAST(@sourceIds AS int);
+ END
+
+ IF @sourceIds IS NULL
+ BEGIN
+  SET @sourceId = NULL;
+ END
+ 
+ IF @orderBy = '' OR @orderBy IS NULL
+ BEGIN
+	IF @centralLatitude IS NOT NULL
+	BEGIN
+		SET @orderBy = 'distance'
+	END
+ ELSE
+	BEGIN
+		SET @orderBy = 'beds'
+	END
+ END
+
+ IF @imperial = 1
+ BEGIN
+	SET @conversion = 1609.344;
+ END
+ ELSE
+ BEGIN
+	SET @conversion = 1000.000;
+ END
+
+ -- all the slow stuff on the outside working on the smallest possible dataset
+ SELECT
+  totalCount = COUNT(1) OVER ()
+  , rowNum = ROW_NUMBER() OVER
+   (ORDER BY CASE
+    WHEN @orderBy = 'distance' THEN POWER(-1, @orderDESC) * (geodata.STDistance(geography::STGeomFromText('POINT(' + CONVERT(varchar(100), @centralLongitude) + ' ' + CONVERT(varchar(100), @centralLatitude) + ')', 4326)) / @conversion)
+    WHEN @orderBy = 'beds'     THEN POWER(-1, @orderDESC) * maximumNumberOfPeople 
+    WHEN @orderBy = 'rating'   THEN POWER(-1, @orderDESC) * ISNULL(averageRating, 0.1)
+   WHEN @orderBy = 'price'    THEN POWER(-1, @orderDESC) * ISNULL(minimumPricePerNight, 100000)
+    ELSE -propertyId
+   END)
+  , name
+  , [description]
+  , latitude
+  , longitude
+  , propertyId
+  , externalURL
+  , numberOfProperBedrooms
+  , maximumNumberOfPeople
+  , averageRating
+  , url
+  , countryCode
+  , cityName
+  , sourceId
+  , externalId
+  , regionName
+  , minimumPricePerNight
+  , currencyCode
+  , (geodata.STDistance(geography::STGeomFromText('POINT(' + CONVERT(varchar(100), @centralLongitude) + ' ' + CONVERT(varchar(100), @centralLatitude) + ')', 4326)) / @conversion) AS distance
+  , [partner]
+  , internalURL
+  , urlSafeName
+  , logoURL
+ FROM
+	 (
+	 -- here is the main select
+	 SELECT
+	  pro.name
+	  , pro.[description]
+	  , pro.latitude
+	  , pro.longitude
+	  , pro.propertyId
+	  , pro.externalURL
+	  , pro.numberOfProperBedrooms
+	  , pro.maximumNumberOfPeople
+	  , pro.averageRating
+	  , ISNULL(pro.thumbnailUrl, pho.url) AS url
+	  , pro.countryCode
+	  , pro.cityName
+	  , pro.sourceId
+	  , pro.externalId
+	  , pro.regionName
+	  , pro.minimumPricePerNight
+	  , pro.currencyCode
+	  , latlong.geodata
+	  , '' AS [partner]
+	  , '' AS internalURL
+	  , '' AS urlSafeName
+	  , '' AS logoURL
+	 FROM dbo.tab_property pro
+	 INNER JOIN dbo.tab_propertyLatLong latlong
+	 ON pro.propertyId = latlong.propertyId
+	 OUTER APPLY (
+	  SELECT  TOP 1 tab_photo.url
+	  FROM    tab_photo
+	  WHERE   tab_photo.propertyId = pro.propertyId
+	 ) pho
+	 WHERE
+	  ( @searchCriteria IS NULL OR pro.cityName LIKE @searchCriteria OR pro.regionName LIKE @searchCriteria )
+	  AND ( @countryCode IS NULL OR pro.countryCode = @countryCode )
+	  AND ( @typeOfProperty IS NULL OR pro.typeOfProperty = @typeOfProperty )
+	  AND ( pro.maximumNumberOfPeople >= ISNULL(@sleeps, 1) )
+	  AND ( @numberOfBedrooms IS NULL OR numberOfProperBedrooms = @numberOfBedrooms )
+	  AND
+	   (
+	   @sourceId IS NULL
+	   OR
+	   sourceId = @sourceId
+	   OR
+	   sourceId IN
+		/*
+		* There is no SELECT ... WHERE ... IN (@variable) construct in T-SQL
+		* so need to convert @sourceIds into a result set which T-SQL can use.
+		* Requires the utils_numbers table
+		*/
+		(
+		SELECT CAST( SUBSTRING(',' + @sourceIds + ',', number + 1
+		 , CHARINDEX(',', ',' + @sourceIds + ',', number + 1) - number -1) AS int )
+		FROM utils_numbers
+		WHERE ( number <= LEN(',' + @sourceIds + ',') - 1 )
+		 AND ( SUBSTRING(',' + @sourceIds + ',', number, 1) = ',' )
+		)
+	   )
+	   AND
+		(
+			(pro.latitude IS NULL OR pro.longitude IS NULL) AND ((@radius IS NULL OR @radius = 0) AND (@swLatitude IS NULL AND @neLatitude IS NULL))
+		OR
+			(
+			-- Radius not passed: restrict to bounding box of min / max latitude and longitude
+			(@radius IS NULL OR @radius = 0) AND (pro.latitude IS NOT NULL AND pro.longitude IS NOT NULL)
+			AND
+			@swLatitude <= pro.Latitude
+			AND
+			@neLatitude >= pro.Latitude
+			AND
+			@swLongitude <= pro.Longitude
+			AND
+			@neLongitude >= pro.Longitude
+			)
+		OR
+			(
+			-- Radius passed: restrict to radial distance from central latitude and longitude
+			(pro.latitude IS NOT NULL AND pro.longitude IS NOT NULL)
+			AND
+			@radius >= (latlong.geodata.STDistance(geography::STGeomFromText('POINT(' + CONVERT(varchar(100), @centralLongitude) + ' ' + CONVERT(varchar(100), @centralLatitude) + ')', 4326)) / @conversion)
+			)
+		)
+	 ) mainselect
+ ORDER BY rowNum
+ /**
+ ORDER BY
+    CASE WHEN @orderBy = 'distance' THEN POWER(-1, @orderDESC) * dbo.func_getLatLongDistance(@centralLatitude, @centralLongitude, pro.latitude, pro.longitude, @imperial)
+		WHEN @orderBy = 'beds'      THEN POWER(-1, @orderDESC) * pro.maximumNumberOfPeople
+		WHEN @orderBy = 'rating'    THEN POWER(-1, @orderDESC) * ISNULL(pro.averageRating, 0.1)
+		WHEN @orderBy = 'price'     THEN POWER(-1, @orderDESC) * ISNULL(pro.minimumPricePerNight, 100000)
+		ELSE -pro.propertyId
+	END
+ **/
+ OFFSET (@RecsPerPage * (@Page - 1)) ROWS
+ FETCH NEXT @RecsPerPage ROWS ONLY;
+
+END
---
+++ holidayHomes/dbo/Tables/tab_propertyLatLong.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[tab_propertyLatLong] (
+    [latlongID]  BIGINT            IDENTITY (1, 1) NOT NULL,
+    [propertyId] BIGINT            NOT NULL,
+    [latitude]   FLOAT (53)        NULL,
+    [longitude]  FLOAT (53)        NULL,
+    [geodata]    [sys].[geography] NULL,
+    CONSTRAINT [PK_tab_propertyLatLong] PRIMARY KEY CLUSTERED ([latlongID] ASC, [propertyId] ASC)
+);
+
+
+GO
+CREATE SPATIAL INDEX [SI_propertyLatLong]
+    ON [dbo].[tab_propertyLatLong] ([geodata]);
+
