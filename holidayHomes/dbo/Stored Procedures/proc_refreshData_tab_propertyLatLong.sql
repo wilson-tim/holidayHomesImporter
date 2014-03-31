e2ee6070876fe4d4472a43fb6dc1b3f58b319f8e
Tim Wilson <tim.wilson@compareandshare.com>
2014-03-31 13:27:03 +0100

    New SP holidayHomes.dbo.proc_refreshData_tab_propertyLatLong

---
+++ holidayHomes/dbo/Stored Procedures/proc_refreshData_tab_propertyLatLong.sql
@@ -0,0 +1,45 @@
+﻿--------------------------------------------------------------------------------------------
+--	2014-03-31 TW
+--	dbo.proc_refreshData_tab_propertyLatLong
+--  
+--  refreshes table dbo.tab_propertyLatLong for new / changed / deleted geographic data
+--		
+-- History
+--	2014-03-31 TW New
+--------------------------------------------------------------------------------------------
+CREATE PROCEDURE [dbo].[proc_refreshData_tab_propertyLatLong]
+AS
+BEGIN
+
+	MERGE dbo.tab_propertyLatLong AS latlong
+	USING dbo.tab_property AS property
+	ON (latlong.propertyId = property.propertyId)
+	-- changed data
+	WHEN MATCHED AND (latlong.latitude <> property.latitude OR latlong.longitude <> property.longitude)
+		THEN UPDATE SET
+			latlong.latitude = property.latitude
+			, latlong.longitude = property.longitude
+			, latlong.geodata = geography::STGeomFromText('POINT(' + CONVERT(varchar(100), property.longitude) + ' ' + CONVERT(varchar(100), property.latitude) + ')', 4326)
+	-- new data
+	WHEN NOT MATCHED BY TARGET THEN
+		INSERT
+			(
+			propertyId
+			, latitude
+			, longitude
+			, geodata
+			)
+		VALUES
+			(
+			property.propertyId
+			, property.latitude
+			, property.longitude
+			, geography::STGeomFromText('POINT(' + CONVERT(varchar(100), property.longitude) + ' ' + CONVERT(varchar(100), property.latitude) + ')', 4326)
+			)
+	-- deleted data
+	WHEN NOT MATCHED BY SOURCE THEN
+		DELETE
+	;
+
+END
+GO
