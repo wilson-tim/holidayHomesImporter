ddb6fe855a68dcb80857a9105fa14f974a3ad777
Tim Wilson <tim.wilson@compareandshare.com>
2014-04-04 12:41:39 +0100

    utils_countryLookup table - new column createdDate with default value getdate()

--- holidayHomes/dbo/Tables/utils_countryLookup.sql
+++ holidayHomes/dbo/Tables/utils_countryLookup.sql
@@ -10,6 +10,9 @@
     [neLatitude]       FLOAT (53)     NULL,
     [neLongitude]      FLOAT (53)     NULL,
     [radius]           INT            CONSTRAINT [DF_utils_countryLookup_radius] DEFAULT ((0)) NOT NULL,
+    [createdDate]      DATETIME       CONSTRAINT [DF_utils_countryLookup_createdDate] DEFAULT (getdate()) NOT NULL,
     CONSTRAINT [PK_utils_countryLookup] PRIMARY KEY CLUSTERED ([id] ASC)
 );
+
+
 
