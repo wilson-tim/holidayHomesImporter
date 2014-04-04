03f68c8e208af0d52b6d5177e9d40817a7e82c3d
Tim Wilson <tim.wilson@compareandshare.com>
2014-04-04 12:08:51 +0100

    Housekeeping - store useful SQL scripts in the repository

---
+++ clrUtilities/enable_CLR_integration.sql
@@ -0,0 +1,10 @@
+﻿-- 03/03/2014 TW Enable CLR integration in MS SQL Server
+
+sp_configure 'show advanced options', 1;
+GO
+RECONFIGURE;
+GO
+sp_configure 'clr enabled', 1;
+GO
+RECONFIGURE;
+GO
---
+++ holidayHomes_build/check_import_progress_query.sql
@@ -0,0 +1,11 @@
+﻿-- 18/02/2014 MC Query to check import progress
+
+SELECT @@ServerName as [server], * 
+FROM holidayHomes_build.import.tab_runLog 
+WHERE runId IN (SELECT max(runId) FROM holidayHomes_build.import.tab_runLog)
+AND messageType = 'error';
+
+SELECT @@ServerName as [server], * 
+FROM holidayHomes_build.import.tab_runLog
+WHERE runId IN (SELECT max(runId) FROM holidayHomes_build.import.tab_runLog)
+ORDER BY runLogId DESC;
