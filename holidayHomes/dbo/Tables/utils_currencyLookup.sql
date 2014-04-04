4f577b36d3821c572312ceb795461d0af38ea096
Tim Wilson <tim.wilson@compareandshare.com>
2014-04-04 14:37:38 +0100

    utils_currencyLookup table - added localId column to provide cross rate functionality

--- holidayHomes/dbo/Tables/utils_currencyLookup.sql
+++ holidayHomes/dbo/Tables/utils_currencyLookup.sql
@@ -1,9 +1,12 @@
 ﻿CREATE TABLE [dbo].[utils_currencyLookup] (
     [id]          NCHAR (3)  NOT NULL,
+    [localId]     NCHAR (3)  NOT NULL,
     [ask]         FLOAT (53) NULL,
     [bid]         FLOAT (53) NULL,
     [rate]        FLOAT (53) NULL,
     [lastUpdated] DATETIME   CONSTRAINT [DF_Table_1_updatedAt] DEFAULT (getdate()) NOT NULL,
-    CONSTRAINT [PK_utils_currencyLookup] PRIMARY KEY CLUSTERED ([id] ASC)
+    CONSTRAINT [PK_utils_currencyLookup] PRIMARY KEY CLUSTERED ([id] ASC, [localId] ASC)
 );
+
+
 
