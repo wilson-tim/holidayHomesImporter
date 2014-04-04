38fc33b0bee56868a170b1b32cf4e24a4e809b3e
Tim Wilson <tim.wilson@compareandshare.com>
2014-04-04 12:45:48 +0100

    utils_currencyLookup table - new

---
+++ holidayHomes/dbo/Tables/utils_currencyLookup.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[utils_currencyLookup] (
+    [id]          NCHAR (3)  NOT NULL,
+    [ask]         FLOAT (53) NULL,
+    [bid]         FLOAT (53) NULL,
+    [rate]        FLOAT (53) NULL,
+    [lastUpdated] DATETIME   CONSTRAINT [DF_Table_1_updatedAt] DEFAULT (getdate()) NOT NULL,
+    CONSTRAINT [PK_utils_currencyLookup] PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
