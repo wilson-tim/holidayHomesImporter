459fd6f507ff0f56d6ebf8864277566574cbe720
Tim Wilson <tim.wilson@compareandshare.com>
2014-04-07 16:50:01 +0100

    New table [holidayHomes_build].[dbo].[homeAwayCoords] to store screen scraped missing HomeAway latitude and longitude values (new automated process developed by James P) (I shall revise the HomeAway data import feed to  bring this data into table staging.tab_property)

---
+++ holidayHomes_build/dbo/dbo.homeAwayCoords.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [dbo].[homeAwayCoords] (
+    [id]         INT        IDENTITY (1, 1) NOT NULL,
+    [externalId] INT        NOT NULL,
+    [latitude]   FLOAT (53) NULL,
+    [longitude]  FLOAT (53) NULL,
+    CONSTRAINT [PK_homeAwayCoords] PRIMARY KEY CLUSTERED ([id] ASC)
+);
+GO
+
+CREATE NONCLUSTERED INDEX [NCI_latlongUpdate] ON [dbo].[homeAwayCoords]
+(
+	[externalId] ASC
+)
+INCLUDE ( 	[latitude],
+	[longitude]) WITH (SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF) ON [PRIMARY];
+GO
+
+CREATE STATISTICS [_dta_stat_565577053_2_4] ON [dbo].[tab_property]([sourceId], [externalId]);
+GO
