acf76186765a6da5ad25f2f72b802391b023fd60
Tim Wilson <tim.wilson@compareandshare.com>
2014-04-08 11:50:37 +0100

    New table [holidayHomes_build].[dbo].[homeAwayCoords] - externalId column should be nvarchar(100)

--- holidayHomes_build/dbo/dbo.homeAwayCoords.sql
+++ holidayHomes_build/dbo/dbo.homeAwayCoords.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[homeAwayCoords] (
     [id]         INT        IDENTITY (1, 1) NOT NULL,
-    [externalId] INT        NOT NULL,
+    [externalId] NVARCHAR(100)        NOT NULL,
     [latitude]   FLOAT (53) NULL,
     [longitude]  FLOAT (53) NULL,
     CONSTRAINT [PK_homeAwayCoords] PRIMARY KEY CLUSTERED ([id] ASC)
@@ -13,7 +13,4 @@ CREATE NONCLUSTERED INDEX [NCI_latlongUpdate] ON [dbo].[homeAwayCoords]
 )
 INCLUDE ( 	[latitude],
 	[longitude]) WITH (SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF) ON [PRIMARY];
-GO
-
-CREATE STATISTICS [_dta_stat_565577053_2_4] ON [dbo].[tab_property]([sourceId], [externalId]);
 GO
