12b8a076815ae872a90eb1cd2fd41a4320fad84f
Tim Wilson <tim.wilson@compareandshare.com>
2014-05-19 12:28:50 +0100

    Checks and preparations for deployment

--- holidayHomes_build/import/Tables/import.tab_country.sql
+++ holidayHomes_build/import/Tables/import.tab_country.sql
@@ -1,12 +1,15 @@
-﻿CREATE TABLE [import].[tab_country] (
+CREATE TABLE [import].[tab_country] (
     [countryNumber] INT            NOT NULL,
     [countryCode3]  NVARCHAR (3)   NOT NULL,
     [countryCode2]  NVARCHAR (2)   NOT NULL,
-    [countryName]   NVARCHAR (255) NOT NULL
+    [countryName]   NVARCHAR (255) NOT NULL,
+    CONSTRAINT [PK_country] PRIMARY KEY CLUSTERED ([countryName] ASC)
 );
 
 
+
+
 GO
-CREATE CLUSTERED INDEX [CX_import_tab_country]
+CREATE UNIQUE NONCLUSTERED INDEX [UIX_country_code2]
     ON [import].[tab_country]([countryCode2] ASC, [countryName] ASC);
 
