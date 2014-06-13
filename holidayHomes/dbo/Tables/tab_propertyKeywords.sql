e9a79ad20ba4bd08e5d77b1360fecefd19ab9ad2
Tim Wilson <tim.wilson@compareandshare.com>
2014-06-13 16:30:41 +0100

    Property keyword search functionality

--- holidayHomes/dbo/Tables/tab_propertyKeywords.sql
+++ holidayHomes/dbo/Tables/tab_propertyKeywords.sql
@@ -1,8 +1,10 @@
 ﻿CREATE TABLE [dbo].[tab_propertyKeywords] (
-    [propertyId]    BIGINT          NOT NULL,
-	[keywords] NVARCHAR(MAX) NULL,
-    [keywordsSoundex] NVARCHAR(MAX) NULL, 
+    [propertyId]      BIGINT         NOT NULL,
+    [keywords]        NVARCHAR (MAX) NULL,
+    [keywordsSoundex] NVARCHAR (MAX) NULL,
     CONSTRAINT [PK_tab_propertyKeywords_propertyId] PRIMARY KEY CLUSTERED ([propertyId] ASC)
 );
+
+
 
 
