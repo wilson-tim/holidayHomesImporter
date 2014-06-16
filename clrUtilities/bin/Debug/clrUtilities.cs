832633fc585bf4099782ac8852df46f52c018ff6
Tim Wilson <tim.wilson@compareandshare.com>
2014-06-16 14:36:35 +0100

    Created CLR UDF for string cleaning - removing HTML mark up and other unwanted characters

--- clrUtilities/bin/Debug/clrUtilities.cs
+++ clrUtilities/bin/Debug/clrUtilities.cs
@@ -1,13 +1,142 @@
-// from: http://sqlblog.com/blogs/adam_machanic/archive/2009/04/28/sqlclr-string-splitting-part-2-even-faster-even-more-scalable.aspx
+/// <summary>
+/// 
+/// SplitString_Multi( ... )
+/// String splitting code copied from
+/// http://sqlblog.com/blogs/adam_machanic/archive/2009/04/28/sqlclr-string-splitting-part-2-even-faster-even-more-scalable.aspx
+/// 
+/// 28/02/2014  MC  New
+/// 
+/// 
+/// cleanString( ... )
+/// Remove HTML mark up and other non-human data from string
+/// Thanks to Erland Sommarskog for his typically insightful explanation of CLR
+/// http://www.sommarskog.se/arrays-in-sql-2005.html#CLR
+/// 
+/// 16/06/2014  TW  New
+/// 
+/// </summary>
+
 using System;
 using System.Collections;
 using System.Data;
 using System.Data.SqlClient;
 using System.Data.SqlTypes;
 using Microsoft.SqlServer.Server;
+using System.Net;
+using System.Text.RegularExpressions;
 
 public partial class UserDefinedFunctions
 {
+    [Microsoft.SqlServer.Server.SqlFunction(
+       IsDeterministic = true
+       )
+    ]
+    public static String cleanString(String dirtyString)
+    {
+        String cleanedString = dirtyString;
+
+        // Clean specific problems: strings starting with "&#711;"
+        if (cleanedString.Length >= 6)
+            {
+            if (cleanedString.Substring(0, 6) == @"&#711;")
+            {
+                cleanedString = cleanedString.Remove(0, 6);
+            }
+        }
+
+        // Clean specific problems: strings starting with "&#271;&#380;&#733;"
+        if (cleanedString.Length >= 18)
+        {
+            if (cleanedString.Substring(0, 18) == @"&#271;&#380;&#733;")
+            {
+                cleanedString = cleanedString.Remove(0, 18);
+            }
+        }
+
+        // Clean specific problems: strings starting with "* ", "- ", "? "
+        if (cleanedString.Length >= 2)
+        {
+            if (cleanedString.Substring(0, 2) == @"* ")
+            {
+                cleanedString = cleanedString.Remove(0, 2);
+            }
+            if (cleanedString.Substring(0, 2) == @"- ")
+            {
+                cleanedString = cleanedString.Remove(0, 2);
+            }
+            if (cleanedString.Substring(0, 2) == @"? ")
+            {
+                cleanedString = cleanedString.Remove(0, 2);
+            }
+        }
+
+        // Clean specific problems: "&bull;" bullet point character
+        cleanedString = cleanedString.Replace(@"&bull;", " ");
+
+        // Clean specific problems: multiple *, +, = characters
+        Match match = Regex.Match(cleanedString, @"\*{2,}");
+        if (match.Success)
+        {
+            cleanedString = Regex.Replace(cleanedString, @"\*{2,}", " ");
+        }
+        match = Regex.Match(cleanedString, @"\+{2,}");
+        if (match.Success)
+        {
+            cleanedString = Regex.Replace(cleanedString, @"\+{2,}", " ");
+        }
+        match = Regex.Match(cleanedString, @"\={2,}");
+        if (match.Success)
+        {
+            cleanedString = Regex.Replace(cleanedString, @"\={2,}", " ");
+        }
+
+        // HtmlDecode
+        // Decode twice to decode data such as "&amp;#39;"
+        cleanedString = WebUtility.HtmlDecode(cleanedString);
+        cleanedString = WebUtility.HtmlDecode(cleanedString);
+
+        // Strip HTML tags
+        cleanedString = StripTagsCharArray(cleanedString);
+
+        // Left trim
+        cleanedString = cleanedString.TrimStart(' ');
+
+        // Replace multiple spaces - better to replace whitespace, see below
+        // cleanedString = Regex.Replace(cleanedString, @"[ ]{2,}", @" ");
+
+        // Replace runs of whitespace with a single space character
+        cleanedString = Regex.Replace(cleanedString, @"\s+", @" ");
+
+        return (cleanedString);
+    }
+
+    public static string StripTagsCharArray(string source)
+    {
+        char[] array = new char[source.Length];
+        int arrayIndex = 0;
+        bool inside = false;
+        for (int i = 0; i < source.Length; i++)
+        {
+            char let = source[i];
+            if (let == '<')
+            {
+                inside = true;
+                continue;
+            }
+            if (let == '>')
+            {
+                inside = false;
+                continue;
+            }
+            if (!inside)
+            {
+                array[arrayIndex] = let;
+                arrayIndex++;
+            }
+        }
+        return new string(array, 0, arrayIndex);
+    }
+
     [Microsoft.SqlServer.Server.SqlFunction(
        FillRowMethodName = "FillRow_Multi",
        TableDefinition = "item nvarchar(4000)"
---
+++ holidayHomes_build/dbo/Functions/dbo.cleanString.sql
@@ -0,0 +1,9 @@
+﻿CREATE FUNCTION [dbo].[cleanString]
+(
+	@dirtyString nvarchar(MAX)
+)
+RETURNS
+	nvarchar(MAX)
+AS
+	EXTERNAL NAME [clrUtilities].[UserDefinedFunctions].[cleanString];
+GO
