1e020508c0f581859bafcae144523ccbef99b93b
Tim Wilson <tim.wilson@compareandshare.com>
2014-07-09 11:34:43 +0100

    Property keyword search revised following testing on staging

--- holidayHomes/dbo/Stored Procedures/propertyKeywordSearch.sql
+++ holidayHomes/dbo/Stored Procedures/propertyKeywordSearch.sql
@@ -3,16 +3,43 @@
 -- Create date: 12/06/2014
 -- Description:	search for a property using keywords
 --
+-- Parameters
+-- @searchCriteria     list of keywords, space delimited (required)
+-- @typeOfProperty     type of property (optional)
+-- @countryCode        country code, ISO 3166-1 alpha-2 (optional)
+-- @localCurrencyCode  local currency code, ISO 4217 (optional)(default value 'GBP')
+-- @sleeps             number of beds (optional)(default value 1)
+-- @maxSleeps          maximum number of beds (optional)
+-- @numberOfBedrooms   number of bedrooms (optional)
+-- @sourceIds          list of partner ids, comma delimited (optional)
+-- @minPrice           minimum rate in GBP (optional)(default value 1)
+-- @maxPrice           maximum rate in GBP (optional)(default value 10000)
+-- @Page INT           page number (required)
+-- @RecsPerPage INT    number of records per page (required)
+--
+-- Result set
+-- Property details ordered by descending rank
+-- Where rank > 1000 then match is on text
+-- Where rank <= 1000 then match is on soundex
+--
 -- History
 --	2014-06-12 TW New
+--  2014-07-08 TW Added additional parameters per dbo.propertySearch
 -- =============================================
 
 CREATE PROCEDURE [dbo].[propertyKeywordSearch]
-  @keywords VARCHAR(8000) = NULL
-, @sourceId INT = NULL
+  @searchCriteria VARCHAR(8000)
+, @typeOfProperty VARCHAR(15) = NULL
+, @countryCode VARCHAR(2) = NULL
 , @localCurrencyCode NVARCHAR(3) = 'GBP'
-, @Page int
-, @RecsPerPage int
+, @sleeps INT = 1
+, @maxSleeps INT = NULL
+, @numberOfBedrooms INT = NULL
+, @sourceIds VARCHAR(200) = NULL
+, @minPrice INT = 1
+, @maxPrice INT = 10000
+, @Page INT
+, @RecsPerPage INT
 
 AS
 
@@ -23,13 +50,14 @@ BEGIN
 	DECLARE
 		  @localRate FLOAT
 		, @searchString VARCHAR(8000)
-		, @searchStringSoundex VARCHAR(8000);
+		, @searchStringSoundex VARCHAR(8000)
+		, @sourceIdCount INT;
 
 	SELECT @searchString = CONVERT(VARCHAR(8000), COALESCE(item, '') + ' ')
 	FROM
 		(
 		SELECT 'FORMSOF(INFLECTIONAL, ' + split.Item + ') AND '
-		FROM dbo.SplitString(@keywords, ' ') AS split
+		FROM dbo.SplitString(@searchCriteria, ' ') AS split
 		WHERE 2 < LEN(split.Item)
 		FOR XML PATH('')
 		) searchStringSelect (item)
@@ -40,7 +68,7 @@ BEGIN
 	FROM
 		(
 		SELECT SOUNDEX(split.Item) + ' AND '
-		FROM dbo.SplitString(@keywords, ' ') AS split
+		FROM dbo.SplitString(@searchCriteria, ' ') AS split
 		WHERE 2 < LEN(split.Item)
 			AND '0000' <> SOUNDEX(split.Item)
 		FOR XML PATH('')
@@ -65,16 +93,40 @@ BEGIN
 		SET @localRate = 1;
 	END
 
+	SET @sleeps = ISNULL(@sleeps, 1);
+ 
+	IF @sourceIds IS NULL OR @sourceIds = ''
+	BEGIN
+	SET @sourceIds = '';
+	END 
+ 
+	SET @sourceIdCount = LEN(@sourceIds) - LEN(REPLACE(@sourceIds, ',', ''));
+	IF LEN(@sourceIds) > 0
+	BEGIN
+	SET @sourceIdCount = @sourceIdCount + 1;
+	END
+
+	IF @minPrice IS NOT NULL
+	BEGIN
+	SET @minPrice = (@minPrice / @localRate);
+	END
+	IF @maxPrice IS NOT NULL
+	BEGIN
+	SET @maxPrice = (@maxPrice / @localRate);
+	END
+
 	SELECT
-		  p.name
+		  totalCount = COUNT(1) OVER ()
+		, p.name
 		, p.[description]
 		, p.latitude
 		, p.longitude
+		, p.propertyId
 		, p.externalURL
 		, p.numberOfProperBedrooms
 		, p.maximumNumberOfPeople
 		, p.averageRating
-		, ISNULL(p.thumbnailUrl, pho.url) AS thumbnailUrl
+		, ISNULL(p.thumbnailUrl, pho.url) AS url
 		, p.countryCode
 		, p.cityName
 		, p.sourceId
@@ -155,17 +207,49 @@ BEGIN
 		AND currency.localId = @localCurrencyCode
 
 	OUTER APPLY (
-	SELECT  TOP 1 dbo.tab_photo.url
-	FROM    dbo.tab_photo
-	WHERE   dbo.tab_photo.propertyId = p.propertyId
+		SELECT  TOP 1 dbo.tab_photo.url
+		FROM    dbo.tab_photo
+		WHERE   dbo.tab_photo.propertyId = p.propertyId
 	) pho
 
 	WHERE
-		(
-		(p.sourceId = @sourceId AND @sourceId IS NOT NULL AND @sourceId <> '')
+		( @typeOfProperty IS NULL OR p.typeOfProperty = @typeOfProperty )
+	AND ( @countryCode IS NULL OR p.countryCode = @countryCode )
+	AND ( @sleeps IS NULL OR p.maximumNumberOfPeople >= @sleeps )
+	AND ( @maxSleeps IS NULL OR p.maximumNumberOfPeople <= @maxSleeps )
+	AND ( @numberOfBedrooms IS NULL OR P.numberOfProperBedrooms = @numberOfBedrooms )
+	AND
+	(
+		@sourceIdCount = 0
 		OR
-		(@sourceId IS NULL OR @sourceId = '')
+		sourceId IN
+		(
+		SELECT split.Item FROM dbo.SplitString(@sourceIds, ',') AS split
 		)
+	)
+	AND (
+		(
+		@minPrice IS NULL
+		OR
+		@maxPrice IS NULL
+		OR
+		minimumPricePerNight IS NULL
+		OR
+		ABS(minimumPricePerNight) = 0
+		OR
+		currencyCode IS NULL
+		OR
+		currency.rate IS NULL
+		OR
+		currency.rate = 0
+		)
+		OR
+		(
+		@minPrice <= (minimumPricePerNight / currency.rate)
+		AND
+		@maxPrice >= (minimumPricePerNight / currency.rate)
+		)
+	)
 
 	ORDER BY [RANK] DESC, resultsListIds.propertyId ASC
 	OFFSET (@RecsPerPage * (@Page - 1)) ROWS
