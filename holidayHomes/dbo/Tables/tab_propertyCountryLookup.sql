0b4dbd7a98a1b8a3e2dd9fe43de958ba414f7b2c
Tim Wilson <tim.wilson@compareandshare.com>
2014-07-10 16:50:48 +0100

    Testing on staging prior to live deployment of latest web site code

---
+++ holidayHomes/dbo/Tables/tab_propertyCountryLookup.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[tab_propertyCountryLookup] (
+    [sourceId]     INT            NOT NULL,
+    [externalId]   NVARCHAR (100) NOT NULL,
+    [propertyId]   BIGINT         NOT NULL,
+    [latitude]     FLOAT (53)     NOT NULL,
+    [longitude]    FLOAT (53)     NOT NULL,
+    [countryCode2] CHAR (2)       NULL,
+    [dateUpdated]  DATETIME       CONSTRAINT [DF_Table_1_createdAt] DEFAULT (getdate()) NULL,
+    [updated]      BIT        CONSTRAINT [DF_tab_propertyCountryLookup_updated] DEFAULT ((0)) NOT NULL,
+    CONSTRAINT [PK_tab_propertyCountryLookup] PRIMARY KEY CLUSTERED ([sourceId] ASC, [externalId] ASC, [propertyId] ASC)
+);
+
