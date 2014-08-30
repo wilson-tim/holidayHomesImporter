0eb9df7c0523d49cc2dc00fc8e6c5f4f1302817f
Tim Wilson <tim.wilson@compareandshare.com>
2014-08-30 18:29:56 +0100

    Deployment - SSISDB Project LSN 54 - database record statistics recording feature

---
+++ holidayHomes_build/import/Tables/import.tab_sourceHistory.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [import].[tab_sourceHistory]
+(
+	  [runId] INT NOT NULL 
+	, [sourceId] INT NOT NULL
+	, [active] INT DEFAULT 0 NOT NULL
+	, [inactive] INT DEFAULT 0 NOT NULL, 
+      CONSTRAINT [PK_tab_sourceHistory] PRIMARY KEY ([runId], [sourceId] ASC) 
+	, CONSTRAINT [FK_tab_run_sourceHistory] FOREIGN KEY ([runId]) REFERENCES [import].[tab_run] ([runId])
+	, CONSTRAINT [FK_tab_source_sourceHistory] FOREIGN KEY ([sourceId]) REFERENCES [import].[tab_source] ([sourceId])
+)
