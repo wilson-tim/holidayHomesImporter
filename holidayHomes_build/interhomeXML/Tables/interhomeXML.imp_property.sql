0dddb183d430a456ccfe9c9c53115bd7f8678279
Tim Wilson <tim.wilson@compareandshare.com>
2015-06-13 15:51:22 +0100

    Experiment to read Interhome XML property data file creating new SSIS package 'interhomeXML_import' and associated files; flipkey package has also changed but don't know why, but left it included just in case

---
+++ holidayHomes_build/interhomeXML/Tables/interhomeXML.imp_property.sql
@@ -0,0 +1,24 @@
+﻿CREATE TABLE [interhomeXML].[imp_property] (
+    [accommodation_Id] BIGINT         NULL,
+    [code]             NVARCHAR (20)  NULL,
+    [name]             NVARCHAR (255) NULL,
+    [country]          NVARCHAR (10)  NULL,
+    [region]           INT            NULL,
+    [place]            INT            NULL,
+    [zip]              NVARCHAR (10)  NULL,
+    [type]             NVARCHAR (10)  NULL,
+    [details]          NVARCHAR (10)  NULL,
+    [quality]          INT            NULL,
+    [brand]            NVARCHAR (10)  NULL,
+    [pax]              INT            NULL,
+    [sqm]              INT            NULL,
+    [rooms]            INT            NULL,
+    [bedrooms]         INT            NULL,
+    [toilets]          INT            NULL,
+    [bathrooms]        INT            NULL,
+    [pets]             INT            NULL,
+    [runId]            INT            NULL,
+    [fileId]           INT            NULL,
+    [sourceId]         INT            NULL
+);
+
